56df64956314ef0e4ddc43c7ab98cb0ba2d6a956
joses <josesamuelsantos321@hotmail.com>
2025-08-13 20:19:30 -0300

    RequestMappin,RequestParam,RestControler,controller,model

---
+++ src/main/java/br/com/bthirtyeight/controllers/GreetingController.java
@@ -0,0 +1,31 @@
+package br.com.bthirtyeight.controllers;
+
+import br.com.bthirtyeight.model.Greeting;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@RestController//define uma classe como controller
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    //estrutura de um url
+    //host/endpoint/parametro=valor
+    //http://localhost:8080/gretting?name=Ricardo
+
+    //para que seja reconhecido como metodo exposto por http
+    @RequestMapping("/gretting")//para definir o nome do endpoint
+    public Greeting greeting (
+                        //requisita o parametro da url
+                        //  value-> pega o valor do parametro
+                        //  defaultValue -> define um valor do parametro padrao caso nao encontre nenhum
+                        @RequestParam(value = "name",defaultValue = "World")
+                        String name
+                             ) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
+}
