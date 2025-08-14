33f690fcf2a49af836b6890e9229367d28da9933
JoseSamueldeMeloSantos <josesamuelsantos321@hotmail.com>
2025-08-14 20:24:39 -0300

    retorno de listas obj em json

--- src/main/java/br/com/bthirtyeight/controllers/PersonController.java
+++ src/main/java/br/com/bthirtyeight/controllers/PersonController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.awt.*;
+import java.util.List;
 
 @RestController
 @RequestMapping("/person")
@@ -19,11 +20,20 @@ public class PersonController {
     private PersonServices services;
 
     @RequestMapping(value = "/{id}",
-            method = RequestMethod.GET,
-            produces = MediaType.APPLICATION_JSON_VALUE
+            method = RequestMethod.GET,//Define que esse método a requisições HTTP do tipo GET
+            produces = MediaType.APPLICATION_JSON_VALUE//Diz que o tipo de resposta gerado por esse endpoint será JSON
     )
     public Person findById(@PathVariable("id") String id) {
            return services.findById(id);
+    }
+
+
+    @RequestMapping(//quando nao tiver o parametro value sera para pega toda a lista json(default)
+            method = RequestMethod.GET,//Define que esse método a requisições HTTP do tipo GET
+            produces = MediaType.APPLICATION_JSON_VALUE//Diz que o tipo de resposta gerado por esse endpoint será JSON
+    )
+    public List<Person> findAll() {
+        return services.findAll();
     }
 
 
