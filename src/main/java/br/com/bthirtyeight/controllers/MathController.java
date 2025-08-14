a7b1999d5fb378216e1d5b780fd995fe048ab32b
joses <josesamuelsantos321@hotmail.com>
2025-08-13 21:37:22 -0300

    definindo metodo de validacao

--- src/main/java/br/com/bthirtyeight/controllers/MathController.java
+++ src/main/java/br/com/bthirtyeight/controllers/MathController.java
@@ -13,10 +13,33 @@ public class MathController {
     @RequestMapping("/sum/{numberOne}/{numberTwo}")
     public Double sum(
             //pathVariable -> puxa a variavel certa do parametro
-            @PathVariable("numberOne") String numerOne,
-            @PathVariable("numberTwo") String numerTwo
+            @PathVariable("numberOne") String numberOne,
+            @PathVariable("numberTwo") String numberTwo
     ) {
-        return 1.0;
+        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+            throw new IllegalArgumentException();
+        }
+
+
+        return convertToDouble(numberOne) + convertToDouble(numberTwo);
     }
 
+    public Double convertToDouble(String strNumber) {
+        if(strNumber == null || strNumber.isEmpty()) {
+            throw new IllegalArgumentException();
+        }
+        String number = strNumber.replaceAll(",",".");
+
+        return Double.parseDouble(number);
+    }
+
+    public boolean isNumeric(String strNumber) {
+
+        if (strNumber == null || strNumber.isEmpty()) {
+            return false;
+        }
+        String number =  strNumber.replaceAll(",", ".");
+
+        return number.matches("[-+]?[0-9]*\\.?[0-9]+");
+    }
 }
