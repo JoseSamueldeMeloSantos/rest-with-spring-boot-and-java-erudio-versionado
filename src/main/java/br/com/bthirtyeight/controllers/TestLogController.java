962f47ffd943e4cf4b6c4ca68aa8c8da11deba13
JoseSamueldeMeloSantos <josesamuelsantos321@hotmail.com>
2025-08-16 17:58:17 -0300

    trabalhando com config de logs em pacotes especificos

---
+++ src/main/java/br/com/bthirtyeight/controllers/TestLogController.java
@@ -0,0 +1,26 @@
+package br.com.bthirtyeight.controllers;
+
+import br.com.bthirtyeight.services.PersonServices;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class TestLogController {
+
+    //correto e imporr do slf4j
+    private Logger logger = LoggerFactory.getLogger(TestLogController.class.getName());
+
+
+    @GetMapping("/test")
+    public String testLog() {
+        logger.debug("this is an Debug log");
+        logger.info("this is an Info log");
+        logger.warn("this is an warn log");
+        logger.error("this is an Error log");
+        return "Logs generated successfully";
+    }
+}
